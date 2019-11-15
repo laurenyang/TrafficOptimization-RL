8444b8246ef92a0f22487037197e8e43148cceae
laurenyang <lauren2yang@gmail.com>
2019-11-15 00:46:33 -0800

    adding images

--- visualize.py
+++ visualize.py
@@ -1,16 +1,30 @@
+# Simple pygame program
+
+# Import and initialize the pygame library
 import pygame
-
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('CS238 Final Project')
-clock = pygame.time.Clock()
 
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
 
-crashed = False
-while not crashed:
-for event in pygame.event.get():
-	if event.type == pygame.QUIT:
-	    crashed = True
-	print(event)
-pygame.display.update()
-clock.tick(60)
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((0,200,0))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+    pygame.display.update()
+
+# Done! Time to quit.
+pygame.quit()
