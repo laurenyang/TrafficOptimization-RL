0a5d9f90af88786376e34ca01b65dd790b361834
tommyy96 <tommy2yang@gmail.com>
2019-11-15 01:17:03 -0800

    add background pygame

--- visualize.py
+++ visualize.py
@@ -5,10 +5,26 @@ import pygame
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode((900,900))
+pygame.display.flip()
+
+background_image = pygame.image.load("images/road.png").convert_alpha()
+background_image = pygame.transform.scale(background_image, (900, 900))
+
+car = pygame.image.load("images/car.png").convert_alpha()
+car = pygame.transform.scale(car, (75, 40)) 
+
+screen.fill([0,255,0])
+screen.blit(background_image, [0, 0])
+
+screen.blit(car, [450,452])
+screen.blit(car, [0,412])
+
+pygame.display.update()
 
 # Run until the user asks to quit
 running = True
+print('running')
 while running:
 
     # Did the user click the window close button?
@@ -16,15 +32,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
-    screen.fill((0,200,0))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
     # Flip the display
-    pygame.display.flip()
-    pygame.display.update()
+    # pygame.display.update()
 
 # Done! Time to quit.
 pygame.quit()
