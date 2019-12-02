a724ef156ea801c82cdc473466256b19492cda1a
tommyy96 <tommy2yang@gmail.com>
2019-12-02 00:34:56 -0800

    convert visualize to class

--- visualize.py
+++ visualize.py
@@ -1,101 +1,117 @@
-# Simple pygame program
-
-# Import and initialize the pygame library
 import pygame
-import random
-pygame.init()
 
-# Set up the drawing window
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 900
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+class Visualize:
+    # Constants
+    SCREEN_WIDTH = 900
+    SCREEN_HEIGHT = 900
+    ROAD_WIDTH = 100
+    CAR_LENGTH = 60
+    CAR_WIDTH = 32
+    LIGHT_WIDTH = 24
+    LIGHT_HEIGHT = 60
+    BACKGROUND_COLOR = [0, 150, 50]
 
-transparent = (0, 0, 0, 0)
+    RIGHT_AXIS = SCREEN_HEIGHT // 2 + ROAD_WIDTH // 4 - CAR_WIDTH // 2
+    LEFT_AXIS = SCREEN_HEIGHT // 2 - ROAD_WIDTH // 4 - CAR_WIDTH // 2
+    UP_AXIS = SCREEN_WIDTH // 2 + ROAD_WIDTH // 4 - CAR_WIDTH // 2
+    DOWN_AXIS = SCREEN_WIDTH // 2 - ROAD_WIDTH // 4 - CAR_WIDTH // 2
 
-background_image = pygame.image.load("images/road.png").convert_alpha()
-background_image = pygame.transform.scale(background_image, (900, 900))
+    RIGHT_START = 0
+    LEFT_START = SCREEN_WIDTH - CAR_LENGTH
+    UP_START = SCREEN_HEIGHT - CAR_LENGTH
+    DOWN_START = 0
 
-right_axis = 452
-left_axis = 410
-up_axis = 452
-down_axis = 410
+    LEFT_LIGHT_POS = (SCREEN_WIDTH // 2 - ROAD_WIDTH // 2 - LIGHT_WIDTH, SCREEN_WIDTH // 2 - ROAD_WIDTH // 2)
+    RIGHT_LIGHT_POS = (SCREEN_WIDTH // 2 + ROAD_WIDTH // 2, SCREEN_WIDTH // 2 + ROAD_WIDTH // 2 - LIGHT_HEIGHT)
+    UP_LIGHT_POS = (SCREEN_WIDTH // 2 + ROAD_WIDTH // 2 - LIGHT_HEIGHT, SCREEN_WIDTH // 2 - ROAD_WIDTH // 2 - LIGHT_WIDTH)
+    DOWN_LIGHT_POS = (SCREEN_HEIGHT // 2 - ROAD_WIDTH // 2, SCREEN_HEIGHT // 2 + ROAD_WIDTH // 2)
 
-right_start = 0
-left_start = 900
-up_start = 900
-down_start = 0
+    def __init__(self):
+        """
+        Initialize the visualization screen.
+        """
+        self.pygame = pygame
+        self.pygame.init()
+        
+        self.screen = self.pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        
+        self.background_image = self.pygame.image.load("images/road.png").convert_alpha()
+        self.background_image = self.pygame.transform.scale(background_image, (900, 900))
 
-car = pygame.image.load("images/car.png").convert_alpha()
-left_car = [pygame.transform.scale(car, (75, 40)), [left_start, left_axis]]
-right_car = [pygame.transform.flip(left_car[0], True, False), [right_start, right_axis]]
-down_car = [pygame.transform.rotate(left_car[0], 90), [down_axis, down_start]]
-up_car = [pygame.transform.flip(down_car[0], False, True), [up_axis, up_start]]
+        self.car = self.pygame.image.load("images/car.png").convert_alpha()
+        self.left_car = [self.pygame.transform.scale(self.car, (self.CAR_LENGTH, self.CAR_WIDTH)), [self.LEFT_START, self.LEFT_AXIS]]
+        self.right_car = [self.pygame.transform.flip(self.left_car[0], True, False), [self.RIGHT_START, self.RIGHT_AXIS]]
+        self.down_car = [self.pygame.transform.rotate(self.left_car[0], 90), [self.DOWN_AXIS, self.DOWN_START]]
+        self.up_car = [self.pygame.transform.flip(self.down_car[0], False, True), [self.UP_AXIS, self.UP_START]]
 
-red_light = pygame.image.load("images/redlight.png").convert_alpha()
-green_light = pygame.image.load("images/greenlight.png").convert_alpha()
-right_red = pygame.transform.scale(red_light, (60, 60))
-right_green = pygame.transform.scale(green_light, (60, 60))
-left_red = pygame.transform.flip(right_red, False, True)
-left_green = pygame.transform.flip(right_green, False, True)
-down_red = pygame.transform.rotate(left_red, 90)
-down_green = pygame.transform.rotate(left_green, 90)
-up_red = pygame.transform.rotate(right_red, 90)
-up_green = pygame.transform.rotate(right_green, 90)
+        self.red_light = self.pygame.image.load("images/redlight.png").convert_alpha()
+        self.green_light = self.pygame.image.load("images/greenlight.png").convert_alpha()
+        self.right_red = self.pygame.transform.scale(self.red_light, (self.LIGHT_WIDTH, self.LIGHT_HEIGHT))
+        self.right_green = self.pygame.transform.scale(self.green_light, (self.LIGHT_WIDTH, self.LIGHT_HEIGHT))
+        self.left_red = self.pygame.transform.flip(self.right_red, False, True)
+        self.left_green = self.pygame.transform.flip(self.right_green, False, True)
+        self.down_red = self.pygame.transform.rotate(self.left_red, 90)
+        self.down_green = self.pygame.transform.rotate(self.left_green, 90)
+        self.up_red = self.pygame.transform.rotate(self.right_red, 90)
+        self.up_green = self.pygame.transform.rotate(self.right_green, 90)
 
-left_light_pos = (350, 400)
-right_light_pos = (490, 440)
-up_light_pos = (440, 360)
-down_light_pos = (400, 490)
+        self.screen.fill(self.BACKGROUND_COLOR)
+        self.screen.blit(self.background_image, [0, 0])
 
-screen.blit(right_green, left_light_pos)
-screen.blit(left_green, right_light_pos)
-screen.blit(up_red, up_light_pos)
-screen.blit(down_red, down_light_pos)
+        self.screen.blit(self.right_car[0], self.right_car[1])
+        self.screen.blit(self.left_car[0], self.left_car[1])
+        self.screen.blit(self.down_car[0], self.down_car[1])
+        self.screen.blit(self.up_car[0], self.up_car[1])
 
-screen.fill([0,150,50])
-screen.blit(background_image, [0, 0])
+        self.screen.blit(self.right_green, self.RIGHT_LIGHT_POS)
+        self.screen.blit(self.left_green, self.LEFT_LIGHT_POS)
+        self.screen.blit(self.up_red, self.UP_LIGHT_POS)
+        self.screen.blit(self.down_red, self.DOWN_LIGHT_POS)
 
-screen.blit(right_car[0], right_car[1])
-screen.blit(left_car[0], left_car[1])
-screen.blit(down_car[0], down_car[1])
-screen.blit(up_car[0], up_car[1])
+        self.pygame.display.update()
 
-pygame.display.update()
+    def quit(self):
+        """
+        Ends the pygame instance
+        """
+        self.pygame.quit()
 
-# Run until the user asks to quit
-running = True
-print('running')
-while running:
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # Flip the display
-    pygame.display.update()
+    def delay(self, time):
+        """
+        Delays window by time milliseconds
+        """
+        self.pygame.time.delay(time)
 
-    # Update and wait
-    right_car[1][0] += random.randint(10,30)
-    left_car[1][0] -= random.randint(10,30)
-    up_car[1][1] -= random.randint(10,30)
-    down_car[1][1] += random.randint(10,30)
+    def update(self, right_cars, left_cars, up_cars, down_cars):
+        """
+        Updates cars in window
+        """
+        self.screen.fill([0,150,50])
+        self.screen.blit(background_image, [0, 0])
 
-    if right_car[1][0] > SCREEN_WIDTH:
-        right_car[1][0] = right_start
-    if left_car[1][0] < 0:
-        left_car[1][0] = left_start
-    if up_car[1][1] < 0:
-        up_car[1][1] = up_start
-    if down_car[1][1] > SCREEN_HEIGHT:
-        down_car[1][1] = down_start
+        # Add cars moving to the right to the screen
+        for car in right_cars:
+            self.right_car[1][0] = int(car.pos[0] / 0.25 * SCREEN_WIDTH / 2) + SCREEN_WIDTH / 2
+            self.screen.blit(right_car[0], right_car[1])
 
-    screen.fill([0,150,50])
-    screen.blit(background_image, [0, 0])
+        # Add cars moving to the left to the screen
+        for car in left_cars:
+            self.left_car[1][0] = int(car.pos[0] / 0.25 * SCREEN_WIDTH / 2) + SCREEN_WIDTH / 2
+            self.screen.blit(left_car[0], left_car[1])
 
-    screen.blit(right_car[0], right_car[1])
-    screen.blit(left_car[0], left_car[1])
-    screen.blit(down_car[0], down_car[1])
-    screen.blit(up_car[0], up_car[1])
-    pygame.time.delay(100)
+        # Add cars moving downwards to the screen
+        for car in down_cars:
+            self.down_car[1][1] = int(car.pos[1] / 0.25 * SCREEN_HEIGHT / 2) + SCREEN_HEIGHT / 2
+            self.screen.blit(down_car[0], down_car[1])
 
-# Done! Time to quit.
-pygame.quit()
+        # Add cars moving upwards to the screen
+        for car in up_cars:
+            self.up_car[1][1] = int(car.pos[1] / 0.25 * SCREEN_HEIGHT / 2) + SCREEN_HEIGHT / 2
+            self.screen.blit(up_car[0], up_car[1])
+
+        self.screen.blit(right_green, right_light_pos)
+        self.screen.blit(left_green, left_light_pos)
+        self.screen.blit(down_red, down_light_pos)
+        self.screen.blit(up_red, up_light_pos)
+
+        self.pygame.display.update()
