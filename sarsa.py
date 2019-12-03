f53905446098793e144f6ffec0edec310c636a0d
tommyy96 <tommy2yang@gmail.com>
2019-12-02 22:52:52 -0800

    completed sarsa

--- sarsa.py
+++ sarsa.py
@@ -1,5 +1,18 @@
 import utils
+import numpy as np
 import env
 
-def sarsa_lambda(rewards, actions, Lambda):
-        pass
+def sarsa_lambda():
+    # epsilon greedy exploration strategy
+    epochs = 10e5
+    alpha = 0.05
+    gamma = 0.9
+    intersection = Environment()
+    action = intersection.chooseAction(intersection.currState)
+    for _ in range(epochs):
+        r = intersection.reward(intersection.currState, action)
+        prevAction = action
+        action = intersection.chooseAction(intersection.currState)
+        intersection.step()
+        intersection.QTable[(intersection.prevState, prevAction)] = intersection.QTable[(intersection.prevState, prevAction)] * (1 - alpha) 
+                                                                  + alpha * (r + gamma * intersection.QTable[(intersection.currState, action)])
