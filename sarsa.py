b0f02fc74fa68f61f0423430b3d42d70e71c7a3c
tigs <tgsun@stanford.edu>
2019-12-04 23:18:49 -0800

    running

--- sarsa.py
+++ sarsa.py
@@ -9,7 +9,7 @@ import pickle
 def sarsa(intersection):
     timesteps = 1000
     alpha = 0.1
-    gamma = 0.75
+    gamma = 0.85
     action = intersection.chooseAction(intersection.currState)
     for _ in range(timesteps):
         r = intersection.reward(intersection.currState, action)
@@ -22,19 +22,23 @@ def sarsa(intersection):
 def driver():
     epochs = int(2e5)
     intersection = env.Environment()
+    fname = '/dfs/scratch0/tigs/traffic/qtable_tiger_small_alpha1_gamma85.pkl'
     start = time.time()
     for i in range(epochs):
         if i % 1000 == 0:
             print(i)
             print(f'time passed: {time.time() - start} seconds')
             print(len(intersection.QTable))
-            if i % 1000 == 0:
-                f = open('/dfs/scratch0/tigs/traffic/qtable_tiger_small_many_iter.pkl', 'wb')
-                pickle.dump(intersection.QTable, f)
-                f.close()
+            f = open(fname, 'wb')
+            pickle.dump(intersection.QTable, f)
+            f.close()
 
         sarsa(intersection)
         intersection.reset()
+    f = open(fname, 'wb')
+    pickle.dump(intersection.QTable, f)
+    f.close()
+
 
 
 def main():
